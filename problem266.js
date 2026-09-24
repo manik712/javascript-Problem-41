e91071f90fa71ad32b34dfe6b8ce7dccea8b0530
manik712 <ranineela71@gmail.com>
2026-09-24 23:18:28 +0600

    take a string and return number of vowels

--- problem266.js
+++ problem266.js
@@ -1,3 +1,22 @@
-function add(){
-  
+//Create a function that takes a string and returns
+//the number (count) of vowels contained within it.
+
+// Examples
+// countVowels("Celebration") ➞ 5
+// countVowels("Palm") ➞ 1
+// countVowels("Prediction") ➞ 4
+
+// Notes
+// a, e, i, o, u are considered vowels (not y).
+// All test cases are one word and only contain letters.
+
+function countVowels(str) {
+  //console.log(str)
+  //console.log(typeof(str))
+  let x = new RegExp(/[a,e,i,o,u]/g);
+  //console.log(x)
+  let vowels = str.match(x);
+  // console.log(vowels)
+  return vowels ? vowels.length : 0;
 }
+console.log(countVowels("apple"));//2
