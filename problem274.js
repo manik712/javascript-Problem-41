8ce68c7bc5a2df39095dcb144adcbb3b4ecdb243
manik712 <ranineela71@gmail.com>
2026-09-25 23:49:58 +0600

    add tarnary oparator

---
+++ problem274.js
@@ -0,0 +1,16 @@
+//Create a function that finds the word "bomb" in the given string 
+//(not case sensitive). If found, return "Duck!!!", otherwise return "There is no bomb, relax.".
+
+
+// Examples
+// bomb("There is a bomb.") ➞ "Duck!!!"
+
+// bomb("This goes boom!!!") ➞ "There is no bomb, relax."
+
+function bomb(str){
+   let findBomb = str.match(/bomb/gi)  ? "Duck!!!" : "There is no bomb, relax";
+   return findBomb;
+
+}
+console.log(bomb("There is a Bomb.")) //Duck!!!
+console.log(bomb("This goes boom!!!")) //There is no bomb, relax
