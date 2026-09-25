31a303053904740990ad21aa5d2db19ef584950b
manik712 <ranineela71@gmail.com>
2026-09-25 22:40:05 +0600

    return the sum of the absolute value

---
+++ problem272.js
@@ -0,0 +1,35 @@
+//Take an array of integers (positive or negative or both) and
+//return the sum of the absolute value of each element.
+
+// getAbsSum([2, -1, 4, 8, 10]) ➞ 25
+function isAbsNumber(arr) {
+  let sum = 0;
+  //for loop
+  for (let i = 0; i <= arr.length - 1; i++) {
+    sum += arr[i];
+  }
+  //if else condition...
+  if (sum < 0) {
+    return -1 * sum;
+  } else {
+    return sum;
+  }
+}
+
+console.log(isAbsNumber([-3, 4, 6, 8, -9, -6, -5]));
+
+
+
+//alternative   
+
+function isAbsNumber(arr) {
+  let sum = 0;
+  //for loop
+  for (let i = 0; i <= arr.length - 1; i++) {
+    sum += arr[i];
+  }
+
+  return Math.abs(sum);
+}
+
+console.log(isAbsNumber([-3, 4, 6, 8, -9, -6, -5]));
