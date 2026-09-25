94165cba0cd0ee9b6e90097e79d7eab4e117c829
manik712 <ranineela71@gmail.com>
2026-09-25 22:05:37 +0600

    average of all elements in the array is a whole number or not

--- problem271.js
+++ problem271.js
@@ -3,12 +3,22 @@
 
 function isWholeNumber(arr) {
   let sum = 0;
+
   //for loop ...
   for (let i = 0; i <= arr.length - 1; i++) {
     sum += arr[i];
   }
+
   //return the result..
-  return sum;
+
+  let average = sum / arr.length;
+  console.log(average);
+
+  if (Number.isInteger(average)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(isWholeNumber([1, 4, 7, 8]));
+console.log(isWholeNumber([5, 5, 5]));
