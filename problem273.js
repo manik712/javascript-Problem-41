487fb48615272b1ae9b6d3229327c02d080107cf
manik712 <ranineela71@gmail.com>
2026-09-25 23:27:23 +0600

    add null,undefined,false

--- problem273.js
+++ problem273.js
@@ -2,9 +2,20 @@
 //a new array, sorted in ascending order (smallest to biggest).
 
 function sortNumsAscending(arr) {
-	//sort function ....
-  let sortArray = arr.sort((a, b) => a - b);
-  return sortArray;
+  if (arr === null || arr === undefined || arr == []) {
+    return [];
+  } else {
+    //sort function ....
+    let sortArray = arr.sort((a, b) => a - b);
+
+    return sortArray;
+  }
 }
 
-console.log(sortNumsAscending([1, 2, 10, 50, 5]));//[ 1, 2, 5, 10, 50 ]
+console.log(sortNumsAscending(undefined)); //[ 1, 2, 5, 10, 50 ]
+
+!undefined; // true
+!null; // true
+!false; // true
+!0; // true
+!""; // true
